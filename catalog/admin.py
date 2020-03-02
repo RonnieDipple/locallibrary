2961571da9d4ec73bbf6465e501a40fad35d2858
Ronnie <ronniedipple@gmail.com>
2020-03-01 19:52:06 -0600

    html added

--- catalog/admin.py
+++ catalog/admin.py
@@ -11,17 +11,37 @@ admin.site.register(Language)
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
+
+
+# add associated records at the same time. For example,
+# it may make sense to have both the book information and information about the specific copies you've got on the same detail page
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 # decorator to register the models (this does exactly the same thing as the admin.site.register() syntax):
 # Register the Admin classes for Book using the decorator
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+
+
+
 
 # Register the Admin classes for BookInstance using decorator
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
